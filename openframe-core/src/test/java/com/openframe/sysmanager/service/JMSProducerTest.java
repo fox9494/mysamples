07e87c20237127a95bdc41e84981b52f031f33d5
fox9494 <cll219@qq.com>
2014-03-24 16:59:47 +0800

    test

--- openframe-core/src/test/java/com/openframe/sysmanager/service/JMSProducerTest.java
+++ openframe-core/src/test/java/com/openframe/sysmanager/service/JMSProducerTest.java
@@ -22,7 +22,7 @@ import com.openframe.webservice.TestEntity;
  *
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath:/spring/business-config.xml","classpath:/spring/webservice-config.xml"})
+@ContextConfiguration(locations={"classpath:/spring/business-config.xml"})
 @Transactional//执行完数据库的操作会自动回滚
 public class JMSProducerTest {
 	
@@ -39,8 +39,8 @@ public class JMSProducerTest {
     public void sendTest() {  
     	SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");//设置日期格式
 		String start = df.format(new Date());
-    	for (int i = 0; i < 10000; i++) {
-    		producerService.sendMessage(destination, new TestEntity("joy",98));  
+    	for (int i = 0; i < 3; i++) {
+    		producerService.sendMessage(destination, new TestEntity("joy",i));  
 		}
     	System.out.println("start time:"+start);
 		System.out.println("end time:"+df.format(new Date()));
